6a56f92526d02f0b593e094c1dda718f073050cf
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-05-15 21:16:25 -0300

    comite

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) { //consição simples!
+    console.log('voce ultrapassou a velocidade permitida. Multado!')
+}
+console.log('dirija sempre usando cinto de segurança')
