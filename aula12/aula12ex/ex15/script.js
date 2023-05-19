532f92df1df93a735fe6fafd641dee484090177d
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-05-18 21:11:30 -0300

    comited

---
+++ aula12/aula12ex/ex15/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'homem'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'crianca-m.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-velho-m.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'mulher'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'foto-crianca-f.png')
+            } else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'foto-velho-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }   
+        
+}
