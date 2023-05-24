70179f129bcfcbcbe4a5bbb12d2a63632de25de4
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-05-24 17:58:46 -0300

    Update script.js

--- aula14/aula14ex/ex016 copy/script.js
+++ aula14/aula14ex/ex016 copy/script.js
@@ -5,14 +5,26 @@ function contar() {
     var result = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { 
-        window.alert ('[Erro] Faltam dados!')
+        result.innerHTML = 'impossível contar!'
+       //window.alert ('[Erro] Faltam dados!')
     } else {
         result.innerHTML = 'contando: '
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
-        
-        for (var c = i; c <= f; c += p)
-        result.innerHTML += `${c} `
-    }
+        if (p <= 0) {
+            window.alert('Passo inválido! considerando passo 1')
+            p = 1
+        }
+        if (i < f) {
+            for (var c = i; c <= f; c += p) { 
+                result.innerHTML += `${c} \u{1F449}` // o codigo do emoji: U+1F605, o codigo formatado em JS: \u{1F605}
+            }
+        } else {
+            for (var c = i; c >= f; c -= p) {
+                result.innerHTML += `${c} \u{1F449}`
+            }
+        }   
+        result.innerHTML += `\u{1F3C1}`
+    }   
 }
