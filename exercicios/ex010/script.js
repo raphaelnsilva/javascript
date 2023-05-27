b7f8afe1fefba83a2bf79bfc26b976fa7c2b6ac3
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-05-26 22:29:13 -0300

    comite exercicios

---
+++ exercicios/ex010/script.js
@@ -0,0 +1,18 @@
+var resultado = document.querySelector('#res')
+
+function a1() {
+    resultado.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function a2() {
+    resultado.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+
+function a3() {
+    resultado.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function a4() {
+    resultado.innerHTML += '<p>Clicou no quarto botão</p>'
+}
+
