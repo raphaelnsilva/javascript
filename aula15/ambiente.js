9a1c90da29d8364cbf688b69827e69fad80bf2b2
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-06-04 10:46:05 -0300

    fix

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5,8,2,9,3]
+
+num.push(1) //cria uma chave no final e adiciona o valor digitado
+num.sort() //coloca os elementos em ordem crescente
+console.log(num) //mostra os elementos do array
+console.log(`O vetor tem ${num.length} posições`) // mostra quantos elementos tem no array
+console.log(`O primeiro valor do vetor é ${num[0]}`) //num[] mostra o valor do vetor chave
