542435e9eeb01f0a40e283b1173d44b2aa255271
Raphael Silva <129803784+raphaelnsilva@users.noreply.github.com>
2023-06-21 11:28:53 -0300

    commit

--- desafio/d03/index.js
+++ desafio/d03/index.js
@@ -1,10 +1,13 @@
-function mensagem() {
-    let number = Number(window.prompt("Digite um número inteiro qualquer"))
-    window.alert(`o antecessor de ${number} é ${number - 1} e o sucessor de ${number} é ${number + 1}`)
+const resultado = document.querySelector('#result')
+
+function mensagem(Number) {
+    let res = document.createElement("p") 
 }
 
 let initial = document.querySelector("#numero")
 
 initial.addEventListener('click', () => {
-    mensagem()
+    let number = Number(window.prompt("Digite um número inteiro qualquer"))
+    mensagem(Number)
+
 })
